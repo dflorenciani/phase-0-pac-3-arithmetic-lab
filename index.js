278d042c1108aeb733df199c9f5de367e6d43403
Didiel <dflorenciani@gmail.com>
2023-03-24 18:37:43 -0500

    solution

---
+++ index.js
@@ -0,0 +1,72 @@
+1 + 80; //=>
+60 - 40; //=>
+2 * 3.4; //=>
+5.0 / 2.5; //=>
+function add () {
+
+}
+function subtract() {
+
+}
+function multiply() {
+
+}
+function divide() {
+
+}
+function add(a, b) {
+    return "somethinh";
+
+}
+function add(a, b) {
+    return a + b;
+
+}
+function subtract(a, b) {
+    return a - b;
+
+}
+function multiply (a, b) {
+    return a * b;
+
+}
+function divide (a, b) {
+    return a / b;
+
+}
+function increment (n) {
+    return (++n)
+
+}
+function decrement(n) {
+    return (--n)
+
+}
+function makeInt(n) {
+    return(parseInt (n, "10"))
+    
+   
+}
+function preserveDecimal(n) {
+    return parseFloat(n)
+}
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
